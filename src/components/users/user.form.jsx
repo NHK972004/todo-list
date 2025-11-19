168171c416fed6f72eaac3f09a5d505f82921c50
NHKBUILD <ngokhaihoang972004@gmail.com>
2025-11-20 04:42:57 +0700

    complete create user

--- src/components/users/user.form.jsx
+++ src/components/users/user.form.jsx
@@ -1,58 +1,93 @@
 import { UserOutlined } from '@ant-design/icons';
-import { Button, Input, notification } from 'antd';
+import { Button, Input, notification, Modal } from 'antd';
 import { useState } from 'react';
 import { createUserApi, } from '../../services/api.service';
 
-const UserForm = () => {
+const UserForm = (props) => {
+    const { loadUser } = props
+
     const [fullName, setFullName] = useState("");
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("")
-    const [phone, setPhone] = useState("")
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
 
-    const handleClinkButton = async () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleSubmitButton = async () => {
         const res = await createUserApi(fullName, email, password, phone);
         if (res.data) {
             notification.success({
                 message: "Create user",
                 description: "Tạo use thành công"
             })
+            console.log("CHECK RE")
+            resetAndCloseModal();
+            await loadUser();
+
         } else {
             notification.error({
                 message: "Error create user",
                 description: JSON.stringify(res.message)
             })
         }
+    }
 
+    const resetAndCloseModal = () => {
         setFullName("");
         setEmail("");
         setPassword("");
         setPhone("");
+        setIsModalOpen(false);
     }
 
     return (
-        <div className='user-form' style={{ margin: "20px 0" }}>
-            <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
-                <div>
-                    <span>Full name</span>
-                    <Input placeholder=""
-                        value={fullName} onChange={(event) => setFullName(event.target.value)} />
-                </div>
-                <div>
-                    <span>Email</span>
-                    <Input placeholder="" value={email} onChange={(event) => setEmail(event.target.value)} />
-                </div>
-                <div>
-                    <span>Password</span>
-                    <Input.Password placeholder="" value={password} onChange={(event) => setPassword(event.target.value)} />
-                </div>
-                <div>
-                    <span>Phone number</span>
-                    <Input placeholder="" value={phone} onChange={(event) => setPhone(event.target.value)} />
-                </div>
-                <div>
-                    <Button type='primary' onClick={handleClinkButton} htmlType="button">Create User</Button>
-                </div>
+        <div className='user-form' style={{ margin: "10px 0" }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h2>Table User</h2>
+                <Button
+                    onClick={() => setIsModalOpen(true)}
+                    type="primary">Create User</Button>
             </div>
+            <Modal
+                title="Create User"
+                open={isModalOpen}
+                onOk={handleSubmitButton}
+                onCancel={resetAndCloseModal}
+                maskClosable={false}
+                okText="CREATE"
+                cancelText="CANCEL"
+            >
+                <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                    <div>
+                        <span>Full name</span>
+                        <Input placeholder=""
+                            onChange={(event) => setFullName(event.target.value)}
+                            value={fullName}
+                        />
+                    </div>
+                    <div>
+                        <span>Email</span>
+                        <Input placeholder=""
+                            onChange={(event) => setEmail(event.target.value)}
+                            value={email}
+                        />
+                    </div>
+                    <div>
+                        <span>Password</span>
+                        <Input.Password placeholder=""
+                            onChange={(event) => setPassword(event.target.value)}
+                            value={password}
+                        />
+                    </div>
+                    <div>
+                        <span>Phone number</span>
+                        <Input placeholder=""
+                            onChange={(event) => setPhone(event.target.value)}
+                            value={phone}
+                        />
+                    </div>
+                </div>
+            </Modal>
         </div>
     )
 }
--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import UserForm from "../components/users/user.form";
 import UserTable from "../components/users/user.table";
+import { fetchAllUserAPI } from "../services/api.service";
 
 const UserPage = () => {
+    const [dataUsers, setDataUsers] = useState([]);
+
+    useEffect(() => {
+        loadUser();
+    }, [])
+
+    const loadUser = async () => {
+        const res = await fetchAllUserAPI();
+        setDataUsers(res.data)
+    }
+
     return (
         <div style={{ padding: "20px" }}>
-            <UserTable />
-            <UserForm />
+            <UserForm loadUser={loadUser} />
+            <UserTable dataUsers={dataUsers} />
         </div>
     );
 }
